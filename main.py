5b428de6b6ece51ec0c29aa5a4989e7cf78d52de
KoKoLates <the21515@gmail.com>
2026-05-04 23:04:06 -0400

    feat(main): update argument parser

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import argparse
 
 from core import Env
-from planner import PrioritizedPlanner
-from placement import ParticleSwarmOptimizer, GA
-from visualizer import Visualizer, SignalVisualizer
+from planner import Planner, PrioritizedPlanner, CBSPlanner
+from placement import Coverage, ParticleSwarmOptimizer, GA
+from visualizer import SignalVisualizer
+
+
+COV_OPT_REGISTRY: dict[str, Coverage] = {
+    "ga":  GA,
+    "pso": ParticleSwarmOptimizer,
+}
+
+PLANNER_REGISTRY: dict[str, Planner] = {
+    "prioritized": lambda env: PrioritizedPlanner(env, "far"),
+    "cbs":         lambda env: CBSPlanner(env),
+}
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', type=str, help='filepath to map file')
-    parser.add_argument('-o', '--output', type=str)
+    parser = argparse.ArgumentParser(description="Coverage path planning pipeline")
+    parser.add_argument("-f", "--file",    type=str, required=True,
+                        help="filepath to map file")
+    parser.add_argument("-c", "--cov-opt", type=str, default="ga",
+                        choices=COV_OPT_REGISTRY.keys(),
+                        help="coverage optimizer: ga | pso  (default: ga)")
+    parser.add_argument("-p", "--planner", type=str, default="prioritized",
+                        choices=PLANNER_REGISTRY.keys(),
+                        help="path planner: prioritized | cbs  (default: prioritized)")
+    parser.add_argument("-o", "--output",  type=str, default=None,
+                        help="(optional) output file path for animation")
     args = parser.parse_args()
 
-    env = Env(args.file)
-    cov_opt = GA(env)
-    points = cov_opt.process()
-
-    cost_obs, cost_connect, cost_coll, cost_travel, cost_cover, cost_all = cov_opt.compute_metric()
-    
-
-    planner = PrioritizedPlanner(env, 'far')
-    paths = planner.process(points)
+    env: Env            = Env(args.file)
+    cov_opt: Coverage   = COV_OPT_REGISTRY[args.cov_opt](env)
+    points              = cov_opt.process()
+    planner: Planner    = PLANNER_REGISTRY[args.planner](env)
+    paths               = planner.process(points)
 
     viz = SignalVisualizer(env)
     viz.animate(paths, args.output)
